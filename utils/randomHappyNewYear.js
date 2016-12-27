804dfb6ae771d912f6b15182c41f7e146fb28d04
Daniil Globa <daniil.globa@umbrella-web.com>
2016-12-27 14:02:00 +0300

    Let's add some magic

--- utils/randomHappyNewYear.js
+++ utils/randomHappyNewYear.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = () => {
-  const icons = ['snowflake', 'snowman', 'snowman_without_snow', 'champagne', 'christmas_tree'];
+  const icons = ['snowflake', 'snowman', 'snowman_without_snow', 'champagne', 'christmas_tree', 'santa', 'vzhuh'];
   var choose = Math.floor(Math.random() * icons.length);
 
   return icons[choose];
