c7564a4607e7a38eddc17f70c5190702109f6816
Anton Karmazin <anton.karmazin@umbrella-web.com>
2016-12-28 19:36:59 +0300

    betweenDates util

--- utils/betweenDates.js
+++ utils/betweenDates.js
@@ -4,17 +4,17 @@
  *
  * @param from string 'MM/DD'
  * @param to string 'MM/DD'
- *
+ * @param date object Date
  * @returns {boolean}
  */
-module.exports = (from, to) => {
-  const now = new Date();
+module.exports = (from, to, date) => {
+  date = date || new Date();
   const [fromM, fromD] = from.split('/').map(n => +n);
   const [toM,toD ] = to.split('/').map(n => +n);
 
   return (
-    (fromM === now.getMonth() + 1 && now.getDate() > fromD)
+    (fromM === date.getMonth() + 1 && date.getDate() > fromD)
     ||
-    (toM === now.getMonth() + 1 && now.getDate() < toD)
+    (toM === date.getMonth() + 1 && date.getDate() < toD)
   );
 };
