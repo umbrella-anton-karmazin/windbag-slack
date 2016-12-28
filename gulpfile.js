d1147c315e281670d46805acb51bd07f6912f8f7
Anton Karmazin <anton.karmazin@umbrella-web.com>
2016-12-28 14:17:49 +0300

    babel compiler for tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const babel = require('babel-core/register');
 
 gulp.task('default', () => {});
 
 gulp.task('test', cb =>
   gulp.src('./tests/**/*.test.js')
     .pipe(mocha({
-      reporter: 'nyan'
+      reporter: 'spec',
+      compilers: {
+        js: babel
+      }
     }))
     .once('error', () => {
       process.exit(1);
