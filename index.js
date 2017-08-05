02072c625b5d5d2f2077c03880063bcf55978d5a
Anton Karmazin <anton.karmazin@umbrella-web.com>
2017-08-05 17:51:15 +0300

    add express/pug functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const Botkit = require('botkit');
 const handlers = require('./handlers');
+const express = require('express');
+const app = express();
 
 // Create bot with disabled statistics.
 const controller = Botkit.slackbot({
@@ -11,7 +13,16 @@ const controller = Botkit.slackbot({
 
 // connect the bot to a stream of messages
 controller.spawn({
-  token: process.env.SLACK_BOT_TOKEN
+  token: 'xoxb-120066405570-aFNQmZEZ4V8WRkFlbKgzNecH'
 }).startRTM();
 
 handlers(controller);
+
+app.set("view engine", "pug");
+app.set('views', './views');
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+});
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
